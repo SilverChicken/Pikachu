8ad959f5878ea80750b37b00cd6b79b8a5cfec64
SilverChicken <quentin.tang@yahoo.fr>
2017-03-18 14:17:54 +0000

    Add files via upload

--- HackTheBrugh/src/person_input/Employee.java
+++ HackTheBrugh/src/person_input/Employee.java
@@ -6,18 +6,41 @@ import java.util.ArrayList;
 
 public class Employee {
 	
-	String Id = "";
+	int Id;
+	int month,yearsWorked;
 	List<Boolean> lateOrNot = new ArrayList<Boolean>();
 	List<Integer> interRating = new ArrayList<Integer>();
 	HashMap<String,String> actionToContext = new HashMap<String,String>();
 	
 	
-	public Employee(String Id, List<Boolean> lateOrNot, List<Integer> interRating, HashMap<String,String> actionToContext) {
+	public Employee(int Id, int month, int year, List<Boolean> lateOrNot, List<Integer> interRating, HashMap<String,String> actionToContext) {
 		this.Id = Id;
+		this.month = month;
+		this.yearsWorked = year;
 		this.lateOrNot = lateOrNot;
 		this.interRating = interRating;
 		this.actionToContext = actionToContext;
 	}
+	
+	public double averageLate() {
+		double i = 0;
+		double n = (double)lateOrNot.size();
+		for(boolean b : lateOrNot) {
+			i += (b? 1 : 0);
+		}
+		return i / n;
+	}
+	
+	public double averageRating() {
+		double i = 0;
+		double n = (double)lateOrNot.size();
+		for(boolean b : lateOrNot) {
+			i += (b? 1 : 0);
+		}
+		return i / n;
+	}
+	
+	
 		
 	
 }
