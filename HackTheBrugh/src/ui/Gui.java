815017895b956a95a49439e897b9e39f11bed6f2
SilverChicken <quentin.tang@yahoo.fr>
2017-03-19 05:28:06 +0000

    Add files via upload

--- HackTheBrugh/src/ui/Gui.java
+++ HackTheBrugh/src/ui/Gui.java
@@ -10,29 +10,39 @@ import javax.swing.WindowConstants;
 public class Gui extends Frame implements ActionListener {
    private Label lblCount;    // Declare a Label component 
    private Label lblCount2;
-   private Label lbl1 = new Label("Very satisfactory");
-   private Label lbl2 = new Label("Underperforming");
-   private Label lbl3 = new Label("Unsatisfactory");
+   private String lbl1 = "Very satisfactory";
+   private String lbl2 = "Underperforming";
+   private String lbl3 = "Unsatisfactory";
    private TextField IDin;  // Declare a TextField component
    private TextField likely; //Declare a TextField component
+   private TextField annoc;
    private Button proceed;   // Declare a Button component
    private Button close;
-  
+   
+   private Label  lblColum1;
+   private TextField Colum1;
+   private Label  lblColum2;
+   private TextField Colum2;
+   private Label  lblColum3;
+   private TextField Colum3;
+   private Label  lblColum4;
+   private TextField Colum4;
+   
    // Constructor to setup GUI components and event handlers
    public Gui () {
       setLayout(new FlowLayout());
-         // "super" Frame (a Container) sets its layout to FlowLayout, which arranges
-         // the components from left-to-right, and flow to next row from top-to-bottom.
-      
       lblCount = new Label("Please add an ID");  // construct the Label component
-      add(lblCount);                    // "super" Frame adds Label
+      add(lblCount);                    
  
       IDin = new TextField("", 10); // construct the TextField component
-      IDin.setEditable(true);       // set to write
-      add(IDin);                     // "super" Frame adds TextField
+      IDin.setEditable(true);       
+      add(IDin);                    
  
       proceed = new Button("ok");   // construct the Button component
-      add(proceed); // "super" Frame adds Button
+      add(proceed); 
+      
+      close = new Button("close");
+      add(close);
       
       lblCount2 = new Label("Numerical Evaluation of Employee Performance:");
       add(lblCount2); 
@@ -41,32 +51,55 @@ public class Gui extends Frame implements ActionListener {
       likely.setEditable(false);
       add(likely);
       
-    //  close = new Button("Close");
-    //  close.
-    // add(close);
-   
+      annoc = new TextField("",30);
+      annoc.setEditable(false);
+      add(annoc);
+    
+      lblColum1 = new Label("Late:");
+      add(lblColum1);
+      Colum1 = new TextField("",10);
+      Colum1.setEditable(false);
+      add(Colum1);
+      
+      lblColum2 = new Label("Rating:");
+      add(lblColum2);
+      Colum2 = new TextField("",10);
+      Colum2.setEditable(false);
+      add(Colum2);
+
+      lblColum3 = new Label("Response:");
+      add(lblColum3);
+      Colum3 = new TextField("",10);
+      Colum3.setEditable(false);
+      add(Colum3);
+
+      lblColum4 = new Label("Time Spent at the company:");
+      add(lblColum4);
+      Colum4 = new TextField("",10);
+      Colum4.setEditable(false);
+      add(Colum4);
+      
+    
+      close.addActionListener(new ActionListener() {
+    	    public void actionPerformed(ActionEvent e)
+    	    {
+    	        //Execute when button is pressed
+    	        System.exit(0);
+    	    }
+    	});
+      
       proceed.addActionListener(this);
-         // btnCount is the source object that fires ActionEvent when clicked.
-         // The source add "this" instance as an ActionEvent listener, which provides
-         //  an ActionEvent handler called actionPerformed().
-         // Clicking btnCount invokes actionPerformed().
+      
+      
+         
  
       setTitle("You are fired!");  // "super" Frame sets its title
       setSize(450, 300);        // "super" Frame sets its initial window size
  
-      // For inspecting the components/container objects
-      // System.out.println(this);
-      // System.out.println(lblCount);
-      // System.out.println(tfCount);
-      // System.out.println(btnCount);
+     
  
       setVisible(true);         // "super" Frame shows
-      
- 
-      // System.out.println(this);
-      // System.out.println(lblCount);
-      // System.out.println(tfCount);
-      // System.out.println(btnCount);
+
    }
    
 
@@ -83,25 +116,30 @@ public class Gui extends Frame implements ActionListener {
 	  for(Integer k : results.keySet()){
 	  System.out.println(k);
 	  System.out.println(results.get(k));
+	  
+	  
 	  }
    }
    // ActionEvent handler - Called back upon button-click.
    @Override
    public void actionPerformed(ActionEvent evt) {
-	   
-	   
 	   String ID = IDin.getText();
 	   int Id = Integer.parseInt(ID);
 	   if(results.get(Id) != null){
 		   double u = 1 - (Math.round(results.get(Id) * 100.0) / 100.0);
 		   likely.setText(String.valueOf(u));
-		   if(u > 0.6) add(lbl1);
-		   else if( u > 0.2) add(lbl2);
-		   else add(lbl3);
+		   
+		   if(u > 0.6) 
+			   annoc.setText(lbl1);
+		   else if( u > 0.2) 
+			   annoc.setText(lbl2);
+		   else 
+			   annoc.setText(lbl3);
 	   }
-   }
-   public void xPressed(ActionEvent et){
-	   setVisible(false); //you can't see me!
-	      dispose(); //Destroy the JFrame objec
+	   
+	   
+	   
+	   
+	   
    }
 }
