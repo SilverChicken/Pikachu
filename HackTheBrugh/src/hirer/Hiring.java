6094c8af403f71bd66b6c5d6b027b45fc10fd530
BenceV <vbence1234@gmail.com>
2017-03-19 09:05:34 +0000

    Update Hiring.java

--- HackTheBrugh/src/hirer/Hiring.java
+++ HackTheBrugh/src/hirer/Hiring.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Hiring {
 	private static String[] pros;
 	private static String[] cons;
-	private static HashMap<Integer,Double> results = new HashMap<Integer,Double>();
+	private static HashMap<String,Double> results = new HashMap<String,Double>();
 	
-	public static HashMap<Integer,Double> order(List<applicant> apps){
+	public static HashMap<String,Double> order(List<applicant> apps){
 		for(applicant app : apps){
-			results.put(app.getId(), app.assess(pros, cons));
+			results.put(String.valueOf(app.getId()), app.assess(pros, cons));
 		}
 		return results;
 	}
