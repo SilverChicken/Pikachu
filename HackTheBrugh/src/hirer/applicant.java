825e754494a64db9092db3669a7f264de861e9e6
BenceV <vbence1234@gmail.com>
2017-03-19 03:34:09 +0000

    Update applicant.java

--- HackTheBrugh/src/hirer/applicant.java
+++ HackTheBrugh/src/hirer/applicant.java
@@ -5,7 +5,24 @@ public class applicant {
 	String resume;
 	String interview;
 	
-	public applicant(){
+	public applicant(int Id, In resume, In interview ){
+		this.resume = resume.readAll();
+		this.interview = interview.readAll(); //.split(" ");
+	}
+	
+	public double assess(String[] pros, String[] cons) {
+		double good = 0;
+		double bad = 0;
+		for(String s : pros){
+			good += (resume.split(s).length) + (interview.split(s).length) - 2;
+		}
+		good = good / (double)pros.length;
 		
+		for(String s : cons){
+			bad += (resume.split(s).length) + (interview.split(s).length) - 2;
+		}
+		bad = bad / (double)cons.length;
+		
+		return good - bad;
 	}
 }
