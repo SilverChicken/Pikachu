3d1abad6060e42cb6f32052bc39014b14e41a7ad
likhithasai <m.likhisai@gmail.com>
2017-03-19 01:18:36 +0000

    Add files via upload
    id increment implemented

--- HackTheBrugh/src/person_input/Random_data_sheet.java
+++ HackTheBrugh/src/person_input/Random_data_sheet.java
@@ -6,7 +6,9 @@ import firing_simulator.Learn_Table;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,32 +22,37 @@ import java.util.List;
 public class Random_data_sheet {
 
     private static ArrayList<Employee> emps = new ArrayList<Employee>();	 
-        
+    private static int count=0;
 	public static int idgen()
 	{
+		Random rand = new Random();
 		int min = 1000;
-		int max = 9999;
-	        int idg=(int) (Math.random() * (max - min) + min);
+	        int idg= min+count;
+                count++;
 		return idg;
+                
 	}
 	public static int yeargen()
 	{
+		Random rand = new Random();
 		int min = 2000;
 		int max = 2016;
-	        int yearg=(int) ((Math.random() * (max - min)) + min);
+	        int yearg=(int) ((Math.random() * max) + min);
 		return yearg;
 	} 
 	public static int monthgen()
 	{
+		Random rand = new Random();
+		int min = 0;
 		int max = 11;
-	        int monthg=(int)((Math.random() * max));
+	        int monthg=(int)((Math.random() * max) + min);
 		return monthg;
 	} 
 	public static List<Boolean> boolgen()
 	{
 		Random randomBoolean = new Random();
 		ArrayList<Boolean> lateornot = new ArrayList<Boolean>();
-                for (int i = 0; i < 7; i++) 
+                for (int i = 0; i < 40; i++) 
 		{
             		lateornot.add(randomBoolean.nextBoolean());
         	}
@@ -55,17 +62,17 @@ public class Random_data_sheet {
 	{
 		Random rand = new Random();
 		ArrayList<Double> interrating = new ArrayList<Double>();
-	        for (int i = 0; i < 3; i++) 
+	        for (int i = 0; i < 40; i++) 
 		{
             		interrating.add(10 * rand.nextDouble());
         	}
 		return interrating;
 	} 
-	public static HashMap<Integer, Double> mainish () {
-		for(int i=0;i<100;i++)
+	public static void main (String[] args) {
+		for(int i=0;i<40;i++)
                 {
                     emps.add(new Employee(idgen(),monthgen(),yeargen(),boolgen(),interratinggen(),new HashMap<String,String>()));
                 }
-		return Learn_Table.Learn(emps);
+		Learn_Table.Learn(emps);
 	}
 }
